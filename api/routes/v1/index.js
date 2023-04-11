fb4c6b23283a0bddcc9bce1c7b629419f9aed7d7
root <root@ip-172-31-6-115.eu-west-3.compute.internal>
2023-04-11 15:24:16 +0000

    new

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -1,3 +1,20 @@
+// Add this to the VERY top of the first file loaded in your app
+var apm = require('elastic-apm-node').start({
+
+  // Override the service name from package.json
+  // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+  serviceName: 'api-service',
+
+  // Use if APM Server requires a secret token
+  secretToken: 'tf7fFCo2t4EU18Gmej',
+
+  // Set the custom APM Server URL (default: http://localhost:8200)
+  serverUrl: 'https://984d89e88bfa4aacbdc68c356413c3fc.apm.eu-west-3.aws.elastic-cloud.com:443',
+
+  // Set the service environment
+  environment: 'dev'
+})
+
 var express = require('express');
 var router = express.Router();
 
